f02d92450f0fc6c6add94b3d93761e933765f3d1
Thufail-Al-Humaidy <justtholib05@gmail.com>
2024-09-05 21:19:27 +0700

    membuat gallery dan memperbagus yang lain

--- script.js
+++ script.js
@@ -5,25 +5,3 @@ document.getElementById("menubtn").addEventListener("click", function () {
   mobileMenu.classList.toggle("scale-y-100");
 });
 
-// owl carousel script
-    // $('.carousel').owlCarousel({
-        // margin: 20,
-        // loop: true,
-        // autoplayTimeOut: 2000,
-        // autoplayHoverPause: true,
-        // responsive: {
-            // 0:{
-                // items: 1,
-                // nav: false
-            // },
-            // 600:{
-                // items: 2,
-                // nav: false
-            // },
-            // 1000:{
-                // items: 3,
-                // nav: false
-            // }
-        // }
-    // });
-// Initialization for ES Users
