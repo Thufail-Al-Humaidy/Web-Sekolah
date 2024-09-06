8653412b0b4b724348b6dfac64d494695ffc6804
Thufail-Al-Humaidy <justtholib05@gmail.com>
2024-09-06 19:59:31 +0700

    membuat section pertanyaan dan ajakan

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ document.getElementById("menubtn").addEventListener("click", function () {
   mobileMenu.classList.toggle("scale-y-100");
 });
 
+function toggleAnswer(button) {
+      const answerDiv = button.closest('.question').querySelector('.answer');
+      
+      if (answerDiv.style.height) {
+        // If already open, hide it
+        answerDiv.style.height = null;
+      } else {
+        // Get the full height of the content and set it
+        const fullHeight = answerDiv.scrollHeight + "px";
+        answerDiv.style.height = fullHeight;
+      }
+    }
